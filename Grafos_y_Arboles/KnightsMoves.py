9d059d111b33d29ef3ff2b2fbcde20a7b07e3e47
Augusto Paz <augustoxx2015@gmail.com>
2023-07-22 10:18:20 -0300

    Modificaciones en archivo de ajedrez

--- Grafos_y_Arboles/KnightsMoves.py
+++ Grafos_y_Arboles/KnightsMoves.py
@@ -1,9 +1,34 @@
 import numpy as np
 
+def verifyValue(tablero,ejex,ejey) -> bool:
+    if ejex < 0 or ejey < 0 or ejex > len(tablero) or ejey > len(tablero):
+        return False
+    else:
+        return True
+
+def bfs():
+    ...
+
 def movimientos(tablero,ejex,ejey):
+    counter:int = 0
     
+
+
+
+if __name__ == '__main__':
+    tablero = np.zeros((8,8))
+    ejex:int = 4
+    ejey:int = 7
+    tablero[ejey,ejex] = 1 #posición del caballo
+    ObjetivoX = 0
+    ObjetivoY = 1
+    tablero[ObjetivoY,ObjetivoX] = 2 #objetivo
     
+    movimientos(tablero,ejex-2,ejey-1)
+
+    #movimientos(tablero,ejex-2,ejey-1)
     tablero[ejey-1][ejex-2] = 4
+    
     tablero[ejey-1][ejex+2] = 4
     tablero[ejey-2][ejex-1] = 4
     tablero[ejey-2][ejex+1] = 4
@@ -16,15 +41,5 @@ def movimientos(tablero,ejex,ejey):
         
     except:
         ...
-
-
-
-if __name__ == '__main__':
-    tablero = np.zeros((8,8))
-    ejex:int = 4
-    ejey: int = 7
-    tablero[ejey,ejex] = 1 #posición del caballo
-    tablero[1,2] = 2 #objetivo
     
-    movimientos(tablero,ejex,ejey)
     print(tablero)
