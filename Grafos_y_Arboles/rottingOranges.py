dc63659547c3f9e54bca892da5051f7632dfec5f
Augusto Paz <augustoxx2015@gmail.com>
2023-07-24 00:12:31 -0300

    Mejoras en Rotting Oranges

--- Grafos_y_Arboles/rottingOranges.py
+++ Grafos_y_Arboles/rottingOranges.py
@@ -1,23 +1,39 @@
+def verifyCultivo(cultivo):
+    if 1 in cultivo:
+        return True
+    else:
+        return False
+
 def orangesRotting(cultivo):
     cola = ()
     visited = set()
     directions = [(1,0),(0,1),(-1,0),(0,-1)]
     counter:int=0
 
-    for row in range(len(cultivo)):
-        for col in range(len(cultivo[0])):
-            #print(cultivo[row][col])
-            if cultivo[row][col] == 2:
+    while counter < 3:
+        for row in range(len(cultivo)):
+            for col in range(len(cultivo[0])):
+                #print(cultivo[row][col])
+                if cultivo[row][col] == 2 and (row,col) not in visited:
+                    visited.add((row,col))
 
-                for direction in directions:
-                    new_row=row+direction[0]
-                    new_col=col+direction[1]
-                    if new_row < 0 or new_col < 0 or new_row >= len(cultivo) or new_col >= len(cultivo[0]):
-                        continue
-                    elif (new_row,new_col) not in visited and cultivo[new_row][new_col] == 1:
-                        visited.add((new_row,new_col))
-                        #cultivo[new_row][new_col] = 3
-    return visited
+                    for direction in directions:
+                        new_row=row+direction[0]
+                        new_col=col+direction[1]
+                        if new_row < 0 or new_col < 0 or new_row >= len(cultivo) or new_col >= len(cultivo[0]):
+                            continue
+                        elif cultivo[new_row][new_col] == 1:
+                            #visited.add((new_row,new_col))
+                            cultivo[new_row][new_col] = 2
+                        
+                        new_row = 0
+                        new_col = 0
+        
+        counter+=1
+        if verifyCultivo(cultivo):
+            return counter
+                  
+    
 
 if __name__ == '__main__':
         # Ejemplo de matriz de cultivo de naranjas
@@ -26,11 +42,12 @@ if __name__ == '__main__':
         [1, 1, 0],
         [0, 1, 1]
     ]
-    v=orangesRotting(cultivo)
+    days = orangesRotting(cultivo)
     for i in cultivo:
         print(i)
     
-    for j in v:
-        print(j)
+    print("day: ", days)
+    #for j in v:
+    #    print(j)
 
     #print(len(cultivo))
