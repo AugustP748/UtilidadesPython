c9afd20f30f1c1b999a1b62916bb8eec1e76310b
Augusto Paz <augustoxx2015@gmail.com>
2023-07-24 00:35:10 -0300

    Solución de RottingOranges

--- Grafos_y_Arboles/rottingOranges.py
+++ Grafos_y_Arboles/rottingOranges.py
@@ -1,16 +1,23 @@
 def verifyCultivo(cultivo):
-    if 1 in cultivo:
-        return True
-    else:
+    if any(1 in sublist for sublist in cultivo):
         return False
+    else:
+        return True
+
+def UpdateCultivo(cultivo,contamination):
+    for row in range(len(cultivo)):
+            for col in range(len(cultivo[0])):
+                if (row,col) in contamination:
+                    cultivo[row][col] = 2
+    return  
 
 def orangesRotting(cultivo):
-    cola = ()
     visited = set()
     directions = [(1,0),(0,1),(-1,0),(0,-1)]
+    contamination = []
     counter:int=0
 
-    while counter < 3:
+    while counter < 4:
         for row in range(len(cultivo)):
             for col in range(len(cultivo[0])):
                 #print(cultivo[row][col])
@@ -22,14 +29,15 @@ def orangesRotting(cultivo):
                         new_col=col+direction[1]
                         if new_row < 0 or new_col < 0 or new_row >= len(cultivo) or new_col >= len(cultivo[0]):
                             continue
-                        elif cultivo[new_row][new_col] == 1:
-                            #visited.add((new_row,new_col))
-                            cultivo[new_row][new_col] = 2
+                        elif cultivo[new_row][new_col] == 1 and (new_row,new_col) not in contamination:
+                            contamination.append((new_row,new_col))
                         
                         new_row = 0
                         new_col = 0
         
-        counter+=1
+        counter+=1 
+        UpdateCultivo(cultivo,contamination)
+        contamination = []
         if verifyCultivo(cultivo):
             return counter
                   
@@ -37,17 +45,5 @@ def orangesRotting(cultivo):
 
 if __name__ == '__main__':
         # Ejemplo de matriz de cultivo de naranjas
-    cultivo = [
-        [2, 1, 1],
-        [1, 1, 0],
-        [0, 1, 1]
-    ]
-    days = orangesRotting(cultivo)
-    for i in cultivo:
-        print(i)
-    
-    print("day: ", days)
-    #for j in v:
-    #    print(j)
 
-    #print(len(cultivo))
+
